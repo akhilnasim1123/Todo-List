c0f7a48d16d674eb4d94f99d957418d22f1afa73
akhil <akhilnasim029@gmail.com>
2023-02-22 15:17:46 +0530

    todo-list completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const[toDos,setTodos]= useState([])
+  const [toDo,setTodo] = useState('')
+   return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Hey Jan</h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...toDos,{id:Date.now(),text:toDo,status:false}])} className="fas fa-plus"></i>
+      </div>
+    <div className="todos">
+{
+
+  toDos.map((obj)=>{
+
+   return(
+
+    <div className="todo">
+      <div className="left">
+        <input onChange={(e)=>{
+          setTodos(toDos.filter(obj2=>{
+            if (obj2.id === obj.id){
+              obj2.status = e.target.checked
+            }
+            return obj2
+          }))
+        }} value={obj.status} type="checkbox" name="" id="" />
+        <p>{obj.text}</p>
+      </div>
+      <div className="right">
+        <i onClick={()=>{
+          setTodos(toDos.filter(obj2 => obj2.id !== obj.id))
+          alert(`${obj.text} is Deleted successfully`)
+        }} className="fas fa-times"></i>
+      </div>
+    </div>
+
+   )
+  })
+
+}
+{
+  toDos.map((obj)=>{
+    if(obj.status){
+      return(
+        <div className='dataTable'>
+          
+              
+              <h2 style={{color:'black'}}>{obj.text}</h2>
+              <p className='dataId'>Id:{obj.id} <h className='Status'>Status: <span style={{color:'green'}}>Completed</span></h></p>
+            
+        </div>
+        
+      )
+    }
+    return null;
+  })
+}  </div>
     </div>
   );
 }
